bd7f0d9acdcefb5b631f995ff49d78274fd14bb3
Amber Sprenkels <amber@electricdusk.com>
2023-01-19 13:51:55 +0100

    Update deploy_all.sh

--- deploy_all.sh
+++ deploy_all.sh
@@ -3,11 +3,11 @@
 set -e
 
 HOST=suki ./deploy.sh
-HOST=jet ./deploy.sh
-# HOST=suyin-arch ./deploy.sh
-# HOST=ygritte ./deploy.sh
-# HOST=summer ./deploy.sh
-# HOST=ghost ./deploy.sh
-# HOST=sandor ./deploy.sh
-# HOST=lilo ./deploy.sh
-# HOST=shaggydog ./deploy.sh
+HOST=salie ./deploy.sh
+HOST=lavendel ./deploy.sh
+HOST=ygritte ./deploy.sh
+HOST=summer ./deploy.sh
+HOST=ghost ./deploy.sh
+HOST=sandor ./deploy.sh
+HOST=lilo ./deploy.sh
+HOST=shaggydog ./deploy.sh
